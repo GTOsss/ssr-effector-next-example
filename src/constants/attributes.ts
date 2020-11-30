2d35a04b0221b09198b6280633281beda42f8b25
Timofey Goncharov <gtosss@mail.ru>
2020-11-30 20:48:32 +0300

    Bug reproduce.

--- src/constants/attributes.ts
+++
@@ -1,7 +0,0 @@
-export const options = [
-  { label: 'Вес', value: 0 },
-  { label: 'Объем', value: 1 },
-  { label: 'Площадь', value: 2 },
-  { label: 'Единицы', value: 3 },
-  { label: 'Длина', value: 4 },
-];
